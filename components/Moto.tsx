7d62a09203c3a0e20d359431c12dd344201c17de
faranbutt <faranbutt789@gmail.com>
2023-02-14 18:07:52 +0500

    Fixing Bugs

--- components/Moto.tsx
+++ components/Moto.tsx
@@ -8,7 +8,8 @@ export default function Moto(){
             <Flex direction={"row"} mx='70px' px='60px' borderColor={'Window'} borderRadius='30' bgGradient='linear(to-r, green.200, pink.500)'>
                 <VStack p='52px' >
                     <Text fontFamily={'Poppins'} fontSize='5xl'>Program in a Nutshell </Text>
-                    <Text fontFamily={"Poppins"} fontSize='1xl' fontWeight={'extrabold'}>In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program's beginning. It resembles a cross between a corporate venture and an educational project.</Text>
+                    <Text fontFamily={"Poppins"} fontSize='1xl'>In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six 0months of the program&apos;s beginning. It resembles a cross between a corporate venture and an educational project.</Text>
+
                 </VStack>
                 <HStack>
                     <Button size={'lg'} colorScheme='red'>Apply</Button>
